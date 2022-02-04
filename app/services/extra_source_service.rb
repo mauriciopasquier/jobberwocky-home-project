e21b34227842c845a6f47a85927123ea690a37d8
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2022-02-04 08:55:46 -0300

    Service wrapper for jobberwocky-extra-source

---
+++ app/services/extra_source_service.rb
@@ -0,0 +1,25 @@
+# jobberwocky-extra-source wrapper.
+class ExtraSourceService
+  include HTTParty
+
+  class RequestError < StandardError
+  end
+
+  base_uri 'http://localhost:8081'
+
+  attr_accessor :response
+
+  def jobs(query = {})
+    self.response = self.class.get('/jobs', query: query).collect { |job| parse_job(job) }
+  end
+
+  # Parses a job as it comes from the external service, to our format.
+  def parse_job(job)
+    {
+      name: job[0],
+      salary: job[1],
+      country: job[2],
+      skills: job[3]
+    }
+  end
+end
---
+++ test/models/extra_source_service_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class ExtraSourceServiceTest < ActiveSupport::TestCase
+  setup do
+    @service = ExtraSourceService.new
+  end
+
+  test 'it parses a job response' do
+    response = [
+      'Jr Java Developer',
+      '24000',
+      'Argentina',
+      ['Java', 'OOP']
+    ]
+
+    parsed_job = @service.parse_job(response)
+
+    assert parsed_job[:name] == 'Jr Java Developer'
+    assert parsed_job[:salary] == '24000'
+    assert parsed_job[:country] == 'Argentina'
+    assert parsed_job[:skills] == ['Java', 'OOP']
+  end
+end
