499755fb95e8eb96d066e835e65938124019ea91
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2022-02-04 00:13:48 -0300

    Factories for tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,19 @@
-ENV["RAILS_ENV"] ||= "test"
-require_relative "../config/environment"
-require "rails/test_help"
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+require 'rails/test_help'
+require 'minitest/spec'
 
 class ActiveSupport::TestCase
+  # Make factories available in all test cases
+  include FactoryBot::Syntax::Methods
+
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
-
   # Add more helper methods to be used by all tests here...
+end
+
+class Minitest::Spec
+  # Make factories available in all test cases
+  include FactoryBot::Syntax::Methods
 end
